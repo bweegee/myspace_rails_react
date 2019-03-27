96607016ef7e1b3add56740a71176080e3ce8f6b
bweegee <bw33g33@gmail.com>
2019-03-27 14:24:29 -0600

    initial commit

---
+++ client/src/providers/AuthProvider.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from 'axios';
+
+const AuthContext = React.createContext();
+export const AuthConsumer = AuthContext.Consumer;
+
+export class AuthProvider extends React.Component {
+  state = { user: null, };
+
+  handleRegister = () => {
+  }
+
+  handleLogin = () => {
+  }
+
+  handleLogout = () => {
+  }
+
+  redner() {
+    return (
+    )
+  }
+}
