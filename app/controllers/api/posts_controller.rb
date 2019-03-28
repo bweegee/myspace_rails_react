0691c1cf9f7a239b88628efd7d388e4ac603d4e2
bweegee <bw33g33@gmail.com>
2019-03-28 16:13:34 -0600

    Posts Model and Controller added

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,10 @@
+class Api::PostsController < ApplicationController
+  def index
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ test/controllers/api/posts_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class Api::PostsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get api_posts_index_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get api_posts_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get api_posts_destroy_url
+    assert_response :success
+  end
+
+end
