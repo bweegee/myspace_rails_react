1e9b8744274dd52596c8c0479c4f1665745d6faf
bweegee <bw33g33@gmail.com>
2019-03-29 10:05:07 -0600

    Post Model and Controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,10 +1,40 @@
 class Api::PostsController < ApplicationController
+  # before_action :set_user
+  before_action :set_post, only: [:update, :destroy]
+
   def index
+    render json: Post.all
+  end
+
+  def create
+    @post = Post.create(post_params) do |post|
+      post.user = current_user
+    end
+
+    if post.save
+      render json: post
+    else
+      render json: post.errors, status: 422
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  # def set_user
+  #   @user = User.find(params[:user_id])
+  # end
+
+  def set_post
+    @post = User.posts.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:comment)
   end
 end
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import { Box, Heading, } from 'grommet';
 
-const Home = () => (
-  <Box>
-    <Heading>Home</Heading>
-  </Box>
-)
+class Home extends React.Component {
+  state = { posts: [], }
+
+  componentDidMount() {
+    axios.get("/api/posts")
+      .then( res => this.setState({ posts: res.data, }))
+  }
+
+  render() {
+    const { posts, } = this.state
+    return (
+      <Box>
+        <Heading level="3">Your Posts</Heading>
+        { posts }
+      </Box>
+    )
+  }
+}
 
 export default Home
